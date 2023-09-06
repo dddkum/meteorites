5a574989b58c7b0b90dd24cb211970dbb6e160de
Dmitry Kumachev <dkumachev18@gmail.com>
2023-09-06 08:44:41 +0400

    + очки игроков с одинаковыми никами отображаются через запятую

--- src/components/FinalScreen/FinalScreen.js
+++ src/components/FinalScreen/FinalScreen.js
@@ -6,17 +6,36 @@ const FinalScreen = () => {
   const navigate = useNavigate();
   const [scores, setScores] = useState([]);
 
+  const combineMatchingScores = (scores) => {
+    const result = {};
+
+    scores.forEach(({ name, score }) => {
+      if (result[name]) {
+        result[name].push(score);
+      } else {
+        result[name] = [score];
+      }
+    });
+
+    return Object.entries(result)
+      .map(([name, scores]) => ({
+        name,
+        scores: scores.join(", "),
+      }))
+      .sort((a, b) => a.name.localeCompare(b.name));
+  };
+
   useEffect(() => {
     const storedScores = localStorage.getItem("scores");
     if (storedScores) {
       const parsedScores = JSON.parse(storedScores);
-      const sortedScores = parsedScores.sort((a, b) => b.score - a.score);
-      setScores(sortedScores);
+      const combinedScores = combineMatchingScores(parsedScores);
+      setScores(combinedScores);
     }
   }, []);
 
   const handleGoBack = () => {
-    navigate("/"); // замените '/' на путь к экрану, на который Вы хотите вернуться
+    navigate("/");
   };
 
   return (
@@ -34,7 +53,7 @@ const FinalScreen = () => {
           {scores.map((score, index) => (
             <tr key={index}>
               <td>{score.name}</td>
-              <td>{score.score}</td>
+              <td>{score.scores}</td>
             </tr>
           ))}
         </tbody>
